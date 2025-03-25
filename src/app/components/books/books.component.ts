17f29bed1fc04ee034c7059342bba103c9b25476
OlegSovero <a20173075@pucp.edu.pe>
2025-03-24 19:13:53 -0500

    cambios en el modal y buscador

--- src/app/components/books/books.component.ts
+++ src/app/components/books/books.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { BookService } from '../services/book.service';
 import { CommonModule } from '@angular/common';
 import { BookcardComponent } from "../bookcard/bookcard.component"; 
+import { FormsModule } from '@angular/forms';
 
 interface Book {
   id_book: number;
@@ -13,19 +14,30 @@ interface Book {
 
 @Component({
   selector: 'app-books',
-  imports: [CommonModule, BookcardComponent],
+  imports: [CommonModule, BookcardComponent,FormsModule],
   templateUrl: './books.component.html',
   styleUrl: './books.component.css'
 })
 export class BooksComponent implements OnInit {
   books: Book[] = [];
-
+  searchTerm: string = '';
+  filteredBooks: any[] = [];  // Lista de libros filtrados
   constructor(private bookService: BookService) {}
 
   ngOnInit(): void {
     this.bookService.getBooks().subscribe(response => {
       this.books = response.books;
+      this.filteredBooks = this.books
       console.log(this.books); // Verifica si los datos se reciben correctamente
     });
+  }
+
+  // Filtrar libros por nombre o ISBN
+  searchBooks(): void {
+    const term = this.searchTerm.toLowerCase();  // Convertir a minúsculas para no hacer distinción entre mayúsculas/minúsculas
+    this.filteredBooks = this.books.filter(
+      book =>
+        book.name.toLowerCase().includes(term) || book.isbn.includes(term)  // Buscar por nombre o ISBN
+    );
   }
 }
